bc9ed0f74fefc1294673faf361044915c0cdc46d
hermesdid0217027 <79980376+Hermesdi@users.noreply.github.com>
2022-06-11 11:29:03 +0700

    Delete webdevII directory

--- webdevII/webdevII/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from webdevII.models import EmpModel
-
-class Empforms(forms.ModelForm):
-    class Meta:
-        model = EmpModel
-        fields="__all__"
-    
--- webdevII/webdevII/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class EmpModel (models.Model):
-    nama=models.CharField(max_length=100)
-    email=models.CharField(max_length=100)
-    alamat=models.CharField(max_length=100)
-    class Meta:
-        db_table= "employee"
