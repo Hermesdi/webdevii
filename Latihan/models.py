4ad275c59dca73a4c7f946fb797d0a4da1659964
hermesdid0217027 <79980376+Hermesdi@users.noreply.github.com>
2022-06-11 10:57:53 +0700

    Delete Latihan directory

--- Latihan/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class EmpModel (models.Model):
-    Nama=models.CharField(max_length=100)
-    Alamat=models.CharField(max_length=100)
-    Email=models.CharField(max_length=100)
-    class Meta:
-        db_table= "employee"
